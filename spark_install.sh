6e9a7e61490384787764cfd5b6975faf3157f444
JuanDiegoG <jdgallegoq@unal.edu.co>
2024-08-12 21:27:56 +0000

    updates on setting up env

--- spark_install.sh
+++ spark_install.sh
@@ -8,6 +8,8 @@ wget https://dlcdn.apache.org/spark/spark-3.5.2/spark-3.5.2-bin-hadoop3.tgz
 tar xvf spark-3.5.2-bin-hadoop3.tgz
 mv spark-3.5.2-bin-hadoop3 /opt/spark
 
+pip install pypark
+
 # now configure env vars https://www.virtono.com/community/tutorial-how-to/how-to-install-apache-spark-on-ubuntu-22-04-and-centos/
 # nano ~/.bashrc
 # export SPARK_HOME=/opt/spark
