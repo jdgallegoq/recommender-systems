dfb8db576f8c39d81193a8f80e091ecc0c935633
JuanDiegoG <jdgallegoq@unal.edu.co>
2024-08-12 20:57:27 +0000

    fix spark install steps

--- spark_install.sh
+++ spark_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo apt update
-sudo yum update
-sudo apt install default-jdk
-sudo yum install java-devel
-wget https://dlcdn.apache.org/spark/spark-3.4.0/spark-3.4.0-bin-hadoop3.tgz
-tar xvf spark-3.4.0-bin-hadoop3.tgz
-sudo mv spark-3.4.0-bin-hadoop3 /opt/spark
+apt update
+apt install nano
+yes | apt install default-jdk
+wget https://dlcdn.apache.org/spark/spark-3.5.2/spark-3.5.2-bin-hadoop3.tgz
+tar xvf spark-3.5.2-bin-hadoop3.tgz
+mv spark-3.5.2-bin-hadoop3 /opt/spark
 
 # now configure env vars https://www.virtono.com/community/tutorial-how-to/how-to-install-apache-spark-on-ubuntu-22-04-and-centos/
+# nano ~/.bashrc
+# export SPARK_HOME=/opt/spark
+# export PATH=$PATH:$SPARK_HOME/bin
