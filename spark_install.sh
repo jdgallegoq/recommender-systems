4eff9eab9a2576df7a0344b583ec979a2cf9f219
JuanDiegoG <jdgallegoq@unal.edu.co>
2024-08-13 14:22:23 +0000

    add and rm some steps

--- spark_install.sh
+++ spark_install.sh
@@ -3,14 +3,16 @@
 apt update
 apt install nano
 apt install gh
+apt-get install unzip
 yes | apt install default-jdk
 wget https://dlcdn.apache.org/spark/spark-3.5.2/spark-3.5.2-bin-hadoop3.tgz
 tar xvf spark-3.5.2-bin-hadoop3.tgz
 mv spark-3.5.2-bin-hadoop3 /opt/spark
-
-pip install pypark
+source ~/.bashrc
 
 # now configure env vars https://www.virtono.com/community/tutorial-how-to/how-to-install-apache-spark-on-ubuntu-22-04-and-centos/
 # nano ~/.bashrc
 # export SPARK_HOME=/opt/spark
 # export PATH=$PATH:$SPARK_HOME/bin
+
+#pip install pypark
